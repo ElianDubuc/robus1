0d45d2d5543b857c68ebd52d6e5fb2e039263322
DubzyPro <91563516+DubzyPro@users.noreply.github.com>
2021-09-30 15:43:29 -0400

    Création de la fonction mouvement (ne fonctionne pas encore)

--- Robus A/src/main.cpp
+++ Robus A/src/main.cpp
@@ -7,8 +7,31 @@ void setup() {
   BoardInit();
 }
 
+//--------------------------------------------------
+//Prototypes des fonctions
+void movement(int);
+
+//--------------------------------------------------
+//Fonctions
+
 //Séquence principale.
 void loop() {
+  movement(5);
   Serial.write(ENCODER_Read(0));
   delay(2000);
+}
+
+//Séquence de mouvement
+void movement(int dist)
+{
+  int x;
+  MOTOR_SetSpeed(RIGHT, 0.25);
+  MOTOR_SetSpeed(LEFT, 0.24);
+  for(x = 0; x < dist; x++)
+  {
+    delay(2000);
+  }
+  MOTOR_SetSpeed(RIGHT, 0);
+  MOTOR_SetSpeed(LEFT, 0);
+  
 }
