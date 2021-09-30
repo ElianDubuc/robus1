23d631afe29d4224566897d9bf18ece430ac622b
ElianDubuc <elianjdubuc@gmail.com>
2021-09-30 16:12:09 -0400

    Programme de comparaison des valeurs théoriques et réelles

--- Robus B/src/main.cpp
+++ Robus B/src/main.cpp
@@ -3,24 +3,37 @@
 #include <LibRobus.h>
 
 #define PULSES_PAR_TOUR 3200
+#define PULSES_PAR_SEC 7025
+#define DELAY_LOOP 50
+
+int PULSE_PER_DELAY = PULSES_PAR_SEC / 20;
+int loopCnt = 0;
+int pulseTh = 0; //Pulse théorique
 
 void deplacement(float, float);
 
 void setup() {
   // put your setup code here, to run once: :')
   BoardInit();
-  deplacement(1, 1);
+  deplacement(10, 0.65);
+
+  /*
   ENCODER_Reset(LEFT);
-  ENCODER_Reset(RIGHT);
+  MOTOR_SetSpeed(LEFT, 0.65);
+  delay(1000);
+  Serial.println(ENCODER_Read(0));
+  MOTOR_SetSpeed(LEFT, 0);
+*/
 }
 
 void loop() {
   // put your main code here, to run repeatedly: Allo Isa!!!
-  Serial.print(ENCODER_Read(0));
+  //Serial.print(ENCODER_Read(0));
 }
 
 void deplacement(float d, float v)
 {
+
   ENCODER_Reset(LEFT);
   ENCODER_Reset(RIGHT);
 
@@ -35,15 +48,14 @@ void deplacement(float d, float v)
     MOTOR_SetSpeed(RIGHT, -v);
   }
 
+  loopCnt = 0;
+
   while(ENCODER_Read(0) <= d*PULSES_PAR_TOUR)
   {
-    Serial.println(d - ENCODER_Read(0));
-    if((d*PULSES_PAR_TOUR) - ENCODER_Read(0) < (PULSES_PAR_TOUR)/4)
-    {
-      
-      MOTOR_SetSpeed(LEFT, 0.2);
-      MOTOR_SetSpeed(RIGHT, 0.2);
-    }
+    pulseTh = loopCnt * PULSE_PER_DELAY;
+    Serial.print("Théorique: "); Serial.print(pulseTh); Serial.print("\tPratique: "); Serial.println(ENCODER_Read(0));
+    delay(DELAY_LOOP);
+    loopCnt++;
   }
   MOTOR_SetSpeed(LEFT, 0);
   MOTOR_SetSpeed(RIGHT, 0);
