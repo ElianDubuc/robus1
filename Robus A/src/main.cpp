ffdae1c26a78ab9178ae2851aef62d1f1b4c1590
DubzyPro <91563516+DubzyPro@users.noreply.github.com>
2021-09-30 13:52:17 -0400

    Petits changements pour le mouvement de robus A

--- Robus A/src/main.cpp
+++ Robus A/src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <LibRobus.h>
 
+void mouvement(int);
+
 void setup() {
   //Initialisation de la plateforme.
   BoardInit();
@@ -8,5 +10,12 @@ void setup() {
 
 //Séquence principale.
 void loop() {
-  
+  mouvement();
+}
+
+//La fonction mouvement prend une valeur de distnace en cm et
+//la convertis en pulses qui seront envoyés au encodeurs de moteur
+void mouvement(int dist);
+{
+
 }
