3df4da40cfaf076402bfc4b09ddf3d5704d9ad61
ElianDubuc <elianjdubuc@gmail.com>
2021-09-30 11:19:25 -0400

    Code da base Fonctionnel R-A

--- Robus B/src/main.cpp
+++ Robus B/src/main.cpp
@@ -1,10 +1,12 @@
 #include <Arduino.h>
+#include <stdio.h>
 #include <LibRobus.h>
 
 void setup() {
   // put your setup code here, to run once: :')
+  BoardInit();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly: Allo Isa!!! moi cest anna
+  // put your main code here, to run repeatedly: Allo Isa!!!
 }
