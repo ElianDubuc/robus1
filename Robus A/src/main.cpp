36254c6e8fb33886782a083df3b8fdf95841e582
Gab <gabrielvachon3@gmail.com>
2021-10-07 08:56:39 -0400

    Update main.cpp

--- Robus A/src/main.cpp
+++ Robus A/src/main.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <librobus.h>
 
+#define circonference = 23.9389;
+
 void setup() {
   //Initialisation de la plateforme.
   BoardInit();
@@ -10,6 +12,8 @@ void setup() {
 //Prototypes des fonctions
 void movement(int);
 void debugWheels();
+void ajustementPID();
+void avancer(int);
 
 //--------------------------------------------------
 //Fonctions
@@ -17,25 +21,39 @@ void debugWheels();
 //Circ. 23,9389cm
 //Séquence principale.
 void loop() {
-
-    MOTOR_SetSpeed(LEFT, 1);
-    MOTOR_SetSpeed(RIGHT, 0.25);
-
+  avancer(1);
   debugWheels();
 }
 
+//Permet d'avancer tout droit. Unité en cm
+void avancer(int dist)
+{
+  MOTOR_SetSpeed(LEFT, 0.25);
+  MOTOR_SetSpeed(RIGHT, 0.24);
+  ajustementPID();
+}
+
+//Affichage des valeurs des encodeurs
 void debugWheels() {
+  ENCODER_ReadReset(0);
+  ENCODER_ReadReset(1);
   Serial.println(ENCODER_Read(0));
   Serial.println(ENCODER_Read(1));
   Serial.println("--------------------");
   delay(1000);
 }
 
-//Séquence principale.
-void loop() {
-  movement(5);
-  Serial.write(ENCODER_Read(0));
-  delay(2000);
+//
+void ajustementPID()
+{
+  int leftWheelRotation = ENCODER_Read(0);
+  int rightWheelRotation = ENCODER_Read(1);
+
+  while(leftWheelRotation > rightWheelRotation)
+  {
+    MOTOR_SetSpeed(LEFT, 0.49);
+    MOTOR_SetSpeed(RIGHT, 0.5);
+  }
 }
 
 //Séquence de mouvement
