0f20121a5efb08222b7bb70f7f57fca160686a54
samuel <samueltrep@hotmail.com>
2021-10-07 10:45:42 -0400

    tourner

--- Robus B/src/main.cpp
+++ Robus B/src/main.cpp
@@ -1,12 +1,17 @@
 #include <Arduino.h>
 #include <stdio.h>
 #include <LibRobus.h>
+#include <math.h>
 
 #define PULSES_PAR_TOUR 3200
 #define PULSES_PAR_SEC 7025
 #define DELAY_LOOP 50
+#define CONVERTION_PULSE 133.67
 
 #define KI 0.0005
+#define KP 0.0002
+
+#define DIAMETRE 20
 
 int PULSE_PER_DELAY = PULSES_PAR_SEC / 20;
 int loopCnt = 0;
@@ -19,11 +24,14 @@ int pulsePrRight = 0; //Pulse pratique
 int diffRight = 0;
 
 void deplacement(float, float);
+void tourner(float);
+void tournerSurLui(float);
 
 void setup() {
   // put your setup code here, to run once: :')
   BoardInit();
-  deplacement(10, 0.65);
+  tourner(90);
+  //deplacement(10, 0.5);
 
   /*
   ENCODER_Reset(LEFT);
@@ -61,7 +69,7 @@ void deplacement(float d, float v)
   while(ENCODER_Read(LEFT) <= d*PULSES_PAR_TOUR)
   {
     
-    pulseThLeft = loopCnt * PULSE_PER_DELAY;///Ajst Gauche
+    pulseThLeft = loopCnt * PULSE_PER_DELAY;//Ajst Gauche
     pulsePrLeft = ENCODER_Read(LEFT);
     diffLeft = pulseThLeft - pulsePrLeft;
 
@@ -69,10 +77,10 @@ void deplacement(float d, float v)
     pulsePrRight = ENCODER_Read(RIGHT);
     diffRight = pulseThRight - pulsePrRight;
 
-    if(loopCnt > 10){
+    //if(loopCnt > 10){
         MOTOR_SetSpeed(LEFT, v + diffLeft*KI);
         MOTOR_SetSpeed(RIGHT, v + diffRight*KI);
-      }
+      //}
     
 
     Serial.print(millis()); Serial.print("Théorique: "); Serial.print(pulseThLeft); Serial.print("\tPratique G: "); Serial.print(pulsePrLeft); Serial.print("\tPratique D: "); Serial.print(pulsePrRight);Serial.print("\tV Mot G: "); Serial.print(v + diffLeft*KI); Serial.print("\tV Mot D: "); Serial.println(v + diffRight*KI);
@@ -81,4 +89,34 @@ void deplacement(float d, float v)
   }
   MOTOR_SetSpeed(LEFT, 0);
   MOTOR_SetSpeed(RIGHT, 0);
+}
+
+void tourner(float angle)
+{
+  ENCODER_Reset(LEFT);
+  ENCODER_Reset(RIGHT);
+  float arc =  2 * PI * DIAMETRE * (abs(angle)/360);
+  float dist_pulse = arc * CONVERTION_PULSE;
+
+  if(angle > 0)
+  {
+    while(ENCODER_Read(LEFT) <= dist_pulse)
+    {
+      MOTOR_SetSpeed(LEFT, 0.5);
+    }
+    MOTOR_SetSpeed(LEFT, 0);
+  }
+  else
+  {
+    while(ENCODER_Read(RIGHT) <= dist_pulse)
+    {
+      MOTOR_SetSpeed(RIGHT, 0.5);
+    }
+    MOTOR_SetSpeed(RIGHT, 0);
+  }
+}
+
+void tournerSurLui(float angle)
+{
+
 }
