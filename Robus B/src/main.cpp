f4cf2e6d29d3d2e85ae0b6265f4f82536dcbb4b4
ElianDubuc <elianjdubuc@gmail.com>
2021-10-07 10:46:17 -0400

    Déplacement complété ou presque

--- Robus B/src/main.cpp
+++ Robus B/src/main.cpp
@@ -3,27 +3,20 @@
 #include <LibRobus.h>
 
 #define PULSES_PAR_TOUR 3200
-#define PULSES_PAR_SEC 7025
+#define PULSES_PAR_SEC 10000 //7025
 #define DELAY_LOOP 50
 
+#define KP 0.00002
 #define KI 0.0005
 
-int PULSE_PER_DELAY = PULSES_PAR_SEC / 20;
-int loopCnt = 0;
-int pulseThLeft = 0; //Pulse théorique
-int pulsePrLeft = 0; //Pulse pratique
-int diffLeft = 0;
 
-int pulseThRight = 0; //Pulse théorique
-int pulsePrRight = 0; //Pulse pratique
-int diffRight = 0;
 
 void deplacement(float, float);
 
 void setup() {
   // put your setup code here, to run once: :')
   BoardInit();
-  deplacement(10, 0.65);
+  deplacement(5, 0.40);
 
   /*
   ENCODER_Reset(LEFT);
@@ -41,6 +34,23 @@ void loop() {
 
 void deplacement(float d, float v)
 {
+  double PULSE_PER_DELAY = PULSES_PAR_SEC / 20;
+  int loopCnt = 0;
+  double pulseTh = 0; //Pulse théorique
+  double lastPulseTh = 0;
+
+
+  int pulsePrLeft = 0; //Pulse pratique
+  int diffLeft = 0;
+  int lastPulseLeft = 0;
+  int KPDiffPrLeft = 0;
+  int KPDiffLeft = 0;
+
+  int pulsePrRight = 0; //Pulse pratique
+  int diffRight = 0;
+  int lastPulseRight = 0;
+  int KPDiffPrRight = 0;
+  int KPDiffRight = 0;
 
   ENCODER_Reset(LEFT);
   ENCODER_Reset(RIGHT);
@@ -61,21 +71,34 @@ void deplacement(float d, float v)
   while(ENCODER_Read(LEFT) <= d*PULSES_PAR_TOUR)
   {
     
-    pulseThLeft = loopCnt * PULSE_PER_DELAY;///Ajst Gauche
-    pulsePrLeft = ENCODER_Read(LEFT);
-    diffLeft = pulseThLeft - pulsePrLeft;
+    pulseTh = loopCnt * PULSE_PER_DELAY;
 
-    pulseThRight = loopCnt * PULSE_PER_DELAY;//Ajst Droit
+
+    pulsePrLeft = ENCODER_Read(LEFT);///Ajst Gauche
+
+    KPDiffPrLeft = pulsePrLeft - lastPulseLeft;
+    KPDiffLeft = PULSE_PER_DELAY - KPDiffPrLeft;
+
+    lastPulseLeft = pulsePrLeft;
+    diffLeft = pulseTh - pulsePrLeft;
+
+
+//Ajst Droit
     pulsePrRight = ENCODER_Read(RIGHT);
-    diffRight = pulseThRight - pulsePrRight;
 
-    if(loopCnt > 10){
-        MOTOR_SetSpeed(LEFT, v + diffLeft*KI);
-        MOTOR_SetSpeed(RIGHT, v + diffRight*KI);
-      }
+    KPDiffPrRight = pulsePrRight - lastPulseRight;
+    KPDiffRight = PULSE_PER_DELAY - KPDiffPrRight;
+
+    lastPulseRight = pulsePrRight;
+    diffRight = pulseTh - pulsePrRight;
+
+    //if(loopCnt > 10){
+        MOTOR_SetSpeed(LEFT, v + KPDiffLeft*KP + diffLeft*KI);
+        MOTOR_SetSpeed(RIGHT, v + KPDiffRight*KP + diffRight*KI);
+      //}
     
 
-    Serial.print(millis()); Serial.print("Théorique: "); Serial.print(pulseThLeft); Serial.print("\tPratique G: "); Serial.print(pulsePrLeft); Serial.print("\tPratique D: "); Serial.print(pulsePrRight);Serial.print("\tV Mot G: "); Serial.print(v + diffLeft*KI); Serial.print("\tV Mot D: "); Serial.println(v + diffRight*KI);
+    Serial.print(loopCnt); Serial.print("\tThéorique: "); Serial.print(pulseTh); Serial.print("\tPratique G: "); Serial.print(pulsePrLeft); Serial.print("\tPratique D: "); Serial.print(pulsePrRight);Serial.print("\tV Mot G: "); Serial.print(v + diffLeft*KI); Serial.print("\tV Mot D: "); Serial.print(v + diffRight*KI); Serial.print("\tKP Left : "); Serial.print(KPDiffLeft);Serial.print("\tKP Right : "); Serial.println(KPDiffRight);
     delay(DELAY_LOOP);
     loopCnt++;
   }
