cd96c1f0fedd5c8499f02f189c9ff13e7a952c02
samuel <samueltrep@hotmail.com>
2021-11-18 10:31:42 -0500

    New code

---
+++ code/arduino/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <stdio.h>
+#include <LibRobus.h>
+#include <math.h>
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+void setup() 
+{
+  
+}
+
+void loop() 
+{
+
+}
